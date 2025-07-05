187debf4656dcf75fad0df18e2955262b21296e2
Rashi Tharushi <rashitharushi1222@gmail.com>
2025-07-05 15:46:18 +0530

    Created the home page

---
+++ Components/BlogItem.jsx
@@ -0,0 +1,22 @@
+import { assets, blog_data } from '@/Assets/assets'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function BlogItem({title,description,category,image,id}) {
+  return (
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+        <Link href={`blogs/${id}`}>
+          <Image src={image} alt='image1' width={400} height={400} className='border-b border-black '/>
+        </Link>
+        <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-small'>{category}</p>
+        <div className='p-5'>
+          <h5 className='mb-2 text-lg'>{title}</h5>
+          <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
+          <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
+            Reade more <Image src={assets.arrow} alt='arrow' className='ml-2' width={12}/>
+          </Link>
+        </div>
+    </div>
+  )
+}
