5474c497cea8b9c2ca050f9548587c7768c1e57e
Rashi Tharushi <rashitharushi1222@gmail.com>
2025-08-03 13:33:51 +0530

    created the connection between frontend and backend

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,6 +1,8 @@
 'use client'
 import { assets, blog_data } from '@/Assets/assets';
+import Footer from '@/Components/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { use, useEffect, useState } from 'react'
 
 export default function page({params}) {
@@ -25,7 +27,9 @@ export default function page({params}) {
   return (data?<>
     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
-        <Image src={assets.logo} width={180} alt='logo' className='w-[130px] sm:w-auto'/>
+        <Link href={'/'}>
+          <Image src={assets.logo} width={180} alt='logo' className='w-[130px] sm:w-auto'/>
+        </Link>
         <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000'>
           Get Started <Image src={assets.arrow} alt='arrow'/>
         </button>
@@ -43,7 +47,26 @@ export default function page({params}) {
       <p>{data.description}</p>
       <h3 className='my-5 text-[18px] font-semibold'>Step 1: Self-Reflection and Goal Setting</h3>
       <p className='my-3'>Before you manage your lifestyle, you must have a clear undestanding of what you want to achieve.Start by reflecting on your values, aspirations and long term goals </p>
+      <p className='my-3'>Before you manage your lifestyle, you must have a clear undestanding of what you want to achieve.Start by reflecting on your values, aspirations and long term goals </p>
+      <h3 className='my-5 text-[18px] font-semibold'>Step 2: Self-Reflection and Goal Setting</h3>
+      <p className='my-3'>Before you manage your lifestyle, you must have a clear undestanding of what you want to achieve.Start by reflecting on your values, aspirations and long term goals </p>
+      <p className='my-3'>Before you manage your lifestyle, you must have a clear undestanding of what you want to achieve.Start by reflecting on your values, aspirations and long term goals </p>
+      <h3 className='my-5 text-[18px] font-semibold'>Step 3: Self-Reflection and Goal Setting</h3>
+      <p className='my-3'>Before you manage your lifestyle, you must have a clear undestanding of what you want to achieve.Start by reflecting on your values, aspirations and long term goals </p>
+      <p className='my-3'>Before you manage your lifestyle, you must have a clear undestanding of what you want to achieve.Start by reflecting on your values, aspirations and long term goals </p>
+      <h3 className='my-5 text-[18px] font-semibold'>Conclution</h3>
+      <p className='my-3'>Before you manage your lifestyle, you must have a clear undestanding of what you want to achieve.Start by reflecting on your values, aspirations and long term goals </p>
+      <div className='my-24'>
+          <p className='text-black font font-semibold my-4'>Share this article on social media.</p>
+          <div className='flex'>
+              <Image src={assets.facebook_icon} width={50} alt=''/>
+              <Image src={assets.twitter_icon} width={50} alt=''/>
+              <Image src={assets.googleplus_icon} width={50} alt=''/>
+          </div>
+      </div>
+      
     </div>
+    <Footer/>
     </>:<></>
   );
 }
